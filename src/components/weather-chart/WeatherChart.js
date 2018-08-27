a360f899dee9bacff8fe8858cc6a3e56572e6f4b
issa-mour <38791976+issa-mour@users.noreply.github.com>
2018-08-27 12:25:13 +0300

    prepare chart data (#6)

--- src/components/weather-chart/WeatherChart.js
+++ src/components/weather-chart/WeatherChart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function WeatherChart({dt}) {
-	return <div>Chart {dt}</div>;
+function WeatherChart({data}) {
+	return <div>Chart {data.length}</div>;
 }
 
 export default WeatherChart;
