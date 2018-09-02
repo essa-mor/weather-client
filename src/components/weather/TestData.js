912959db06cd1cb6b2b3a59e6163e0a8b35c6b97
issa-mour <issa.mour2@gmail.com>
2018-09-02 14:40:16 +0300

    select first when no selection

---
+++ src/components/weather/TestData.js
@@ -0,0 +1,6 @@
+export const data = [
+	{ dt: 1535274000, day: '2018-08-23 12:00:00', maxTemperature: 29.39, minTemperature: 27.66, img: '01d' },
+	{ dt: 1535275000, day: '2018-08-24 12:00:01', maxTemperature: 30.39, minTemperature: 27.14, img: '01d' },
+	{ dt: 1535276000, day: '2018-08-25 12:00:02', maxTemperature: 31.39, minTemperature: 26.95, img: '01d' },
+	{ dt: 1535277000, day: '2018-08-26 12:00:03', maxTemperature: 32.39, minTemperature: 27.01, img: '02d' },
+	{ dt: 1535278000, day: '2018-08-27 12:00:04', maxTemperature: 33.69, minTemperature: 26.9, img: '03d' }];
--- src/components/weather/WeatherDaysForecastContainer.test.js
+++ src/components/weather/WeatherDaysForecastContainer.test.js
@@ -3,13 +3,7 @@ import { mount } from 'enzyme';
 
 import { WeatherDaysForecastContainer } from './WeatherDaysForecastContainer';
 import { getWeatherDays } from './WeatherDaysApi';
-
-const data = [
-	{ dt: 1535274000, day: '2018-08-23 12:00:00', maxTemperature: 29.39, minTemperature: 27.66, img: '01d' },
-	{ dt: 1535274000, day: '2018-08-24 12:00:00', maxTemperature: 27.14, minTemperature: 27.14, img: '01d' },
-	{ dt: 1535274000, day: '2018-08-25 12:00:00', maxTemperature: 26.95, minTemperature: 26.95, img: '01d' },
-	{ dt: 1535274000, day: '2018-08-26 12:00:00', maxTemperature: 27.01, minTemperature: 27.01, img: '02d' },
-	{ dt: 1535274000, day: '2018-08-27 12:00:00', maxTemperature: 26.9, minTemperature: 26.9, img: '03d' }];
+import { data } from './TestData';
 
 jest.mock('./WeatherDaysApi');
 
@@ -17,10 +11,30 @@ let wrapper;
 describe('WeatherDaysForecastContainer test', () => {
 	it('renders correctly', done => {
 		getWeatherDays.mockImplementation(() => Promise.resolve(data));
-		wrapper = mount(<WeatherDaysForecastContainer />);
+		wrapper = mount(<WeatherDaysForecastContainer history={[]} />);
 		setTimeout(() => {
 			wrapper.update();
 			expect(wrapper.find('.weather_day_indicator')).toHaveLength(5);
+			done();
+		});
+	});
+
+	it('Select first item', done => {
+		getWeatherDays.mockImplementation(() => Promise.resolve(data));
+		wrapper = mount(<WeatherDaysForecastContainer history={[]} />);
+		setTimeout(() => {
+			wrapper.update();
+			expect(wrapper.state().selectedDt).toBe(1535274000);
+			done();
+		});
+	});
+
+	it('Select from history', done => {
+		getWeatherDays.mockImplementation(() => Promise.resolve(data));
+		wrapper = mount(<WeatherDaysForecastContainer history={[]} location={{ pathname: '1535275000' }} />);
+		setTimeout(() => {
+			wrapper.update();
+			expect(wrapper.state().selectedDt).toBe(1535275000);
 			done();
 		});
 	});
