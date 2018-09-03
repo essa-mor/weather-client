82733d6fe7dd6aade65493258080b49aaa3ef2fc
issa-mour <issa.mour2@gmail.com>
2018-09-03 12:18:13 +0300

    test

--- src/components/weather-chart/WeatherChart.js
+++ src/components/weather-chart/WeatherChart.js
@@ -14,6 +14,8 @@ const labelsHeight = 30;
 class WeatherChart extends React.PureComponent {
 	componentDidUpdate() {
 		const path = select('#line');
+		if(path.node() == null) return;
+		console.log('componentDidUpdate');
 		var totalLength = path.node().getTotalLength();
 		const t = transition()
 			.duration(1000)
